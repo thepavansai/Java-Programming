0501dff2506da45abcd6160690b4084a15d058d6
thepavansai <pavansaisriram@gmail.com>
2024-10-13 22:35:22 +0530

    update runtime_polymorphism

--- method_overriding.java
+++ method_overriding.java
@@ -1,6 +1,33 @@
 //Runtime polymorphism
+class Bank{
+    int interest(){
+        return 6;
+    }
+}
+class UnionBank extends Bank{
+    int interest(){
+        return 8;
+    }
+}
+class Axis extends Bank{
+    int interest(){
+        return 9;
+    }
+}
+class State_Bank_Of_India extends Bank{
+    int interest(){
+        return 10;
+    }
+}
 public class method_overriding {
     public static void main(String [] Phantom){
-
+        Bank b= new Bank();
+        System.out.println("Bank base interest : "+b.interest());
+        State_Bank_Of_India s= new State_Bank_Of_India();
+        System.out.println("Sbi interest Rate : "+s.interest());
+        Axis A= new Axis();
+        System.out.println("Axis Bank : "+A.interest());
+        UnionBank u= new UnionBank();
+        System.out.println("Union Bank : "+u.interest());
     }
 }
